bec8e4916d3892b7ac68c611dfbee91c87b6a3d3
Vinod Yadav <Vinod.Yadav@csgi.com>
2021-05-23 20:54:39 +0200

    new changes

--- ZooAssignment/Services/FileReaderService.cs
+++ ZooAssignment/Services/FileReaderService.cs
@@ -10,8 +10,10 @@ using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using ZooAssignment.Interfaces;
-
-
+using CsvHelper;
+using System.Globalization;
+using ZooAssignment.Model;
+using CsvHelper.Configuration;
 
 namespace ZooAssignment.Services
 {
@@ -49,96 +51,26 @@ namespace ZooAssignment.Services
 
             return txtData;
         }
-
-        //public DataTable ReadCSVFile(string path)
-        //{
-        //    //try
-        //    //{
-        //    //    CsvParserOptions csvParserOptions = new CsvParserOptions(false, ';');
-        //    //    CsvUserDetailsMapping csvMapper = new CsvUserDetailsMapping();
-        //    //    CsvParser<classmodel> csvParser = new CsvParser<classmodel>(csvParserOptions, mapper);
-        //    //    var result = csvParser.ReadFromFile(path, Encoding.ASCII).ToList();
-        //    //    foreach (var details in result)
-        //    //    {
-        //    //        details.Result.FoodPercentage = details.Result.FoodPercentage.Replace("%", string.Empty);
-        //    //        animals.Add(details.Result);
-        //    //    }
-        //    //}
-        //    //catch (Exception)
-        //    //{
-
-        //    //    throw;
-        //    //}
-
-
-        //    //return animals;
-
-        //    var csvTable = new DataTable();
-        //    using (var csvReader = new CsvReader(new StreamReader(System.IO.File.OpenRead(@"D:\CSVFolder\CSVFile.csv")), true))
-        //    {
-        //        csvTable.Load(csvReader);
-        //    }
-
-        //    //var config = new CsvConfiguration(CultureInfo.InvariantCulture)
-        //    //{
-        //    //    HasHeaderRecord = false,
-        //    //};
-
-        //    //var reader = new StreamReader(path);
-        //    //var csv = new CsvReader(reader, config);
-
-
-        //    //return csv;
-
-        //}
-
-        public string ReadCSVFile(string path)
+          
+        public List<T> ReadCSVFile<T>(string path) where T : new()
         {
-            DataTable csvData = new DataTable();
-            string jsonString = string.Empty;
-            try
+            List<T> records = new List<T>();
+
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
             {
-                using (TextFieldParser csvReader = new TextFieldParser(path))
-                {
-                    csvReader.SetDelimiters(new string[] { ";" });
-                    csvReader.HasFieldsEnclosedInQuotes = false;
+                HasHeaderRecord = false,
+                MissingFieldFound = null,
+                Delimiter = ";"
+            };
 
-                    string[] colFields;
-                    bool tableCreated = false;
-                    while (tableCreated == false)
-                    {
-                        colFields = csvReader.ReadFields();
-                        //foreach (string column in colFields)
-                        //{
-                        //    DataColumn datecolumn = new DataColumn(column);
-                        //    datecolumn.AllowDBNull = true;
-                        //    csvData.Columns.Add(datecolumn);
-                        //}
-
-                        for (int i = 0; i < colFields.Length; i++)
-                        {
-                            DataColumn datecolumn = new DataColumn("col" + (i + 1));
-                            datecolumn.AllowDBNull = true;
-                            csvData.Columns.Add(datecolumn);
-                        }
-
-                        tableCreated = true;
-                    }
-                    while (!csvReader.EndOfData)
-                    {
-                        csvData.Rows.Add(csvReader.ReadFields());
-                    }
-                }
-            }
-            catch (Exception ex)
+            using (var reader = new StreamReader(path))
+            using (var csv = new CsvReader(reader, config))
             {
-                Console.WriteLine(ex.Message);
-                return "Error:Parsing CSV";
+                records.AddRange(csv.GetRecords<T>());
             }
-            //if everything goes well, serialize csv to json 
-            jsonString = JsonConvert.SerializeObject(csvData);
 
-            return jsonString;
+            return records;
+
         }
 
         public IEnumerable<XElement> ReadXMLFile(string path)
